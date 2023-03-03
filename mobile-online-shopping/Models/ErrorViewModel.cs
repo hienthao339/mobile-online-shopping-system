50d1f2b5152a106bc9bf5af61cdf42e3153fc296
hienthao339 <hienthao338@gmail.com>
2023-03-03 14:08:49 +0700

    del

--- mobile-online-shopping/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace mobile_online_shopping.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
