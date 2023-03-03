e433739df46b987a34f37de43d603b2a30e389bc
hienthao339 <hienthao338@gmail.com>
2023-03-03 13:54:08 +0700

    create-new-project

---
+++ mobile-online-shopping/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mobile_online_shopping.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
