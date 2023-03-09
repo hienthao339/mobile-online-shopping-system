732f11fea94a9dfcab4c8f4c2f72d6586c9b2813
hienthao339 <hienthao338@gmail.com>
2023-03-09 10:23:11 +0700

    ok

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
