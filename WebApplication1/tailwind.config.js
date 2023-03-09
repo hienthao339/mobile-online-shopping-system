9509a55cc0613c64b93284499416f9d4cf15e019
hienthao339 <hienthao338@gmail.com>
2023-03-09 22:17:38 +0700

    fd

--- WebApplication1/tailwind.config.js
+++ WebApplication1/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
         './Views/**/*.cshtml'
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'hero-pattern': "url('/img/hero-pattern.svg')",
+                'footer-texture': "url('/img/footer-texture.png')",
+            }},
     },
     plugins: [],
 }
